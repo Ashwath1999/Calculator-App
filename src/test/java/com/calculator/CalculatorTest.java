d3f0d5d6500ea4521f13ad89cd7d08c5cd778c53
Ashwath <ashwathravi25@gmail.com>
2025-04-11 12:54:06 +0530

    Added multiplication and division logic

--- src/test/java/com/calculator/CalculatorTest.java
+++ src/test/java/com/calculator/CalculatorTest.java
@@ -16,4 +16,19 @@ public class CalculatorTest {
     public void testSubtract() {
         assertEquals(1, calc.subtract(5, 4));
     }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(20, calc.multiply(4, 5));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, calc.divide(10, 5));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivideByZero() {
+        calc.divide(10, 0);
+    }
 }
